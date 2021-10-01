5156cd62b20166cd7b51e226fef2cb5e8f8772fc
MongHanHuang <52921307+MongHanHuang@users.noreply.github.com>
2021-10-01 06:11:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,30 @@
 * imports.js)
 * Code written by Mong-Han Huang, Department of Geology, mhhuang@umd.edu
 */
+
+/*
+MIT License
+
+Copyright (c) 2021 Mong-Han Huang and Alexander L. Handwerger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
  
 var slope_threshold = .5; // unit: degree
 var curv_threshold = -0.005; // unit: m/m^2
